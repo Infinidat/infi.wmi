9eb95cff916c40852ffa4194123d25677f270a2a
Guy Rozendorn <guy@rzn.co.il>
2012-01-06 18:02:29 +0200

    tests

--- tests/test_wmi.py
+++ tests/test_wmi.py
@@ -3,9 +3,13 @@ from infi import unittest, wmi
 class WmiTestCase(unittest.TestCase):
     def test_query_cimv(self):
         client = wmi.WmiClient()
-        self.assertEqual(len(client.execute_query("SELECT * FROM Win32_ComputerSystemProduct")), 1)
+        query = client.execute_query("SELECT * FROM Win32_ComputerSystemProduct")
+        results = [item for item in query]
+        self.assertEqual(len(results), 1)
 
     def test_query_wmi(self):
         client = wmi.WmiClient(r"root\wmi")
-        self.assertEqual(len(client.execute_query("SELECT * FROM MS_SystemInformation")), 1)
+        query = client.execute_query("SELECT * FROM MS_SystemInformation")
+        results = [item for item in query]
+        self.assertEqual(len(results), 1)
 
